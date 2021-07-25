ceccda25f71d266df87c1ad19d2e70bf13c4777f
hostdavies-12425 <86573783+hostdavies-12425@users.noreply.github.com>
2021-07-25 18:24:04 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+const { evaluate } = require("mathjs");
+const { Plugin } = require("powercord/entities");
+
+class Calculator extends Plugin {
+	startPlugin() {
+		powercord.api.commands.registerCommand({
+			command: "calc",
+			description: "Calculate your query",
+			usage: "{query} your query here!",
+			execustor: (args) => ({
+				send: false,
+				result: this.doMath(args.join(" ")),
+			}),
+		});
+	}
+
+	doMath(query) {
+		if (!query) {
+			return "You didn't provide any query!";
+		}
+
+		return {
+			type: "rich",
+			fields: [
+				{
+					name: "Input:",
+					value: query,
+					inline: true,
+				},
+				{
+					name: "Output:",
+					value: evaluate(query).toString(),
+					inline: true,
+				},
+			],
+			color: 0x4cb2c2,
+		};
+	}
+
+	pluginWillUnload() {
+		powercord.api.commands.unregisterCommand("calc");
+	}
+}
+
+module.exports = Calculator;
